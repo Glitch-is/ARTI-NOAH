289d120fa8309f2e993f8337efcca970a7ad40df
Glitch-is <runylh@gmail.com>
2017-03-20 14:53:46 +0000

    Added twitter data

--- train.py
+++ train.py
@@ -6,19 +6,19 @@ import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
-dataset = Dataset("data/test/Arrival.txt")
+dataset = Dataset("data/test/samples.txt")
 xlen = ylen = 25
 xsize = ysize = len(dataset.word2id)
 hidden_size = 512
 embedding_size = 128
 num_layers=3
-save_path="save/arrival/"
+save_path="save/samples/"
 batch_size=5
 epochs=200
-model = Model(xlen=xlen, ylen=ylen, xsize=xsize, ysize=ysize, 
-              hidden_size=hidden_size, 
-              embedding_size=embedding_size, 
-              num_layers=num_layers, 
+model = Model(xlen=xlen, ylen=ylen, xsize=xsize, ysize=ysize,
+              hidden_size=hidden_size,
+              embedding_size=embedding_size,
+              num_layers=num_layers,
               save_path=save_path,
               epochs=epochs)
 
