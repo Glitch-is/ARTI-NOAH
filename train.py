df131422451ff142d3eb381d7bf2aaa71de4795e
James Sigurðarson <jamiees2@gmail.com>
2017-03-27 09:32:20 -0400

    reduce embedding size

--- train.py
+++ train.py
@@ -6,11 +6,11 @@ import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
-dataset = Dataset("data/twitter.txt")
-xlen = ylen = 25
+xlen = ylen = 5
+dataset = Dataset("data/twitter.txt", maxX=xlen, maxY=ylen)
 xsize = ysize = len(dataset.word2id)
 hidden_size = 512
-embedding_size = 64
+embedding_size = 25
 num_layers=3
 save_path="save/twitter/"
 batch_size=40
@@ -23,11 +23,11 @@ model = Model(xlen=xlen, ylen=ylen, xsize=xsize, ysize=ysize,
               save_path=save_path,
               epochs=epochs,
               learning_rate=learning_rate,
-              model_name="blagh",
+              model_name="blagh2",
               train=True)
 
 training = dataset.getTrainingData()
-training_batch = dataset.getBatch(training, batch_size)
+training_batch = dataset.getRandomBatch(training, batch_size)
 test_batch = dataset.getRandomBatch(dataset.getTestingData(), batch_size)
 model.train(training_batch, test_batch)
 
